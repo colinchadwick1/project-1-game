52822eb0970ff31c991c9da7d964b479a6f65def
colinchadwick1 <colinchadwick@hotmail.co.uk>
2021-11-01 10:04:00 +0100

    a

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
+
 function copyImageToCanvas() {
 
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const image = document.getElementById("#goal");
-
+const image = document.getElementById("goal");
 
 ctx.drawImage(
-    image, 0, 0)
+    image, 0, 0, 900, 500)
 }
+
+copyImageToCanvas();
+
+console.log(ctx)
+console.log(canvas)
+console.log(image)
